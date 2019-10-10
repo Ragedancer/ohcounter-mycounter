8722267e07e828ef30a3515caa2041cdc9a09119
Samuel_Groom <ragedancer00@gmail.com>
2019-10-10 17:35:24 -0400

    Clean-Slate

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -7,11 +7,7 @@ class App extends Component {
         return (
             <div>
                 <header>
-                    <svg onClick="toggleHamMenu()" className="menu-button" xmlns="http://www.w3.org/2000/svg" width="48"
-                         height="48" viewBox="0 0 48 48">
-                        <path d="M0 0h48v48H0z" fill="none"></path>
-                        <path d="M6 36h36v-4H6v4zm0-10h36v-4H6v4zm0-14v4h36v-4H6z"></path>
-                    </svg>
+                   
                 </header>
                 <ActionPrompt/>
             </div>
