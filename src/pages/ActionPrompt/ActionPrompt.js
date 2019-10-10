dfb4599076830f2d71fba90f1f8f7ebec06de4e4
Samuel_Groom <ragedancer00@gmail.com>
2019-10-10 17:33:44 -0400

    Clean-Slate

--- src/pages/ActionPrompt/ActionPrompt.js
+++ src/pages/ActionPrompt/ActionPrompt.js
@@ -5,18 +5,6 @@ class ActionPrompt extends Component {
     render() {
         return (
             <div>
-                <h2>What would you like to do?</h2>
-                <div id={"action-selection"} className={"vertical-selection-input-rounded"}>
-                    <div className={"item"}>
-                        <label id={"action-option-label1"}>New Game</label>
-                        <input type={"radio"} id={"action-option1"} name={"action-prompt-selection"} value={"newGame"}/>
-                    </div>
-                </div><br/><br/>
-
-                {/*Button element is Material-UI component it is imported from the */}
-                <Button variant="contained" color="primary">
-                    Select
-                </Button>
 
             </div>
         );
