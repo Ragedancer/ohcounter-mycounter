5579de5201ef9a19e0e5d760d07c7630d9b9549a
ViNguyen15 <fluffywin@gmail.com>
2019-10-15 16:06:58 -0400

    refactoring menu.js

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,6 +1,37 @@
 import React, {Component} from 'react';
 import './App.scss';
 import ActionPrompt from "../ActionPrompt/ActionPrompt";
+import {checkCookie} from "../../utils/cookie";
+// Toggle show on element
+function toggleShow(element) {
+    document.getElementById(element).classList.toggle('show');
+}
+
+// Toggle hide on cover
+function toggleCover(element) {
+    document.getElementById(element).classList.toggle('hide');
+}
+
+
+// Open the hamburger menu
+function toggleHamMenu() {
+    toggleShow('menu');
+    toggleCover('menu-active');
+    toggleMenuOptions();
+}
+
+function toggleMenuOptions() {
+    if (checkCookie(recentGameCookie)) {
+        document.getElementById('menuReturnGame').classList.toggle('hide', false);
+        document.getElementById('menuRandPlayer').classList.toggle('hide', false);
+        document.getElementById('menuResetGame').classList.toggle('hide', false);
+    }
+    else {
+        document.getElementById('menuReturnGame').classList.toggle('hide', true);
+        document.getElementById('menuRandPlayer').classList.toggle('hide', true);
+        document.getElementById('menuResetGame').classList.toggle('hide', true);
+    }
+}
 
 class App extends Component {
     render() {
