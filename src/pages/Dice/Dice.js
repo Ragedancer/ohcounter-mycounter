292af05102be4ed0851706651a8966cb63143f4e
ViNguyen15 <fluffywin@gmail.com>
2019-10-24 20:21:58 -0400

    imported dice successfully

--- src/pages/Dice/Dice.js
+++ src/pages/Dice/Dice.js
@@ -1,5 +1,13 @@
 import React, {Component} from "react";
 import Button from "@material-ui/core/Button";
+import SvgIcon from "@material-ui/core";
+import Dice4 from "./images/dice/d4.svg";
+import Dice6 from "./images/dice/d6.svg";
+import Dice8 from "./images/dice/d8.svg";
+import Dice10 from "./images/dice/d10.svg";
+import Dice12 from "./images/dice/d12.svg";
+import Dice20 from "./images/dice/d20.svg";
+import Coin from "./images/dice/Coin.svg";
 
 class Dice extends Component {
 
@@ -18,15 +26,21 @@ class Dice extends Component {
                     <h1>Hi Guys Dice here</h1>
                     <h1> Konichiwa!!!</h1>
 
-                </div>
-                <body>
+
+
+                <div id = "Dice">
+                    <img src = {Dice4}  />
+                    <img src = {Dice6}  />
+                    <img src = {Dice8}  />
+                    <img src = {Dice10}  />
+                    <img src = {Dice12}  />
+                    <img src = {Dice20}  />
+                    <img src = {Coin}  />
+                    </div>
 
 
 
-
-
-                </body>
-
+            </div>
             </div>
         );
     }
