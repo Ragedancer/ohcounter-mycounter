3b44c0373844d78e81429ccf85d8494288d81cb3
ViNguyen15 <fluffywin@gmail.com>
2019-11-07 17:02:14 -0500

    dice stuff

--- src/pages/Dice/Dice.js
+++ src/pages/Dice/Dice.js
@@ -8,39 +8,40 @@ import Dice10 from "./images/dice/d10.svg";
 import Dice12 from "./images/dice/d12.svg";
 import Dice20 from "./images/dice/d20.svg";
 import Coin from "./images/dice/Coin.svg";
+import {loadFile, loadFileContents} from "../../utils/template";
 
 class Dice extends Component {
 
-
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
+        this.state = {isEmptyState: true}
     }
+    openDice() {}
+    //testing
+    test(){}
+
 
     render() {
-
         return (
-
             <div>
                 <div id = "main">
                     <h1>Hi Guys Dice here</h1>
                     <h1> Konichiwa!!!</h1>
 
-
-
                 <div id = "Dice">
-                    <img src = {Dice4}  />
+                    <img src = {Dice4}
+                    onClick={null}
+                    />
+
                     <img src = {Dice6}  />
                     <img src = {Dice8}  />
                     <img src = {Dice10}  />
                     <img src = {Dice12}  />
                     <img src = {Dice20}  />
                     <img src = {Coin}  />
+
                     </div>
-
-
-
-            </div>
+                </div>
             </div>
         );
     }
