dcf21e21ce9e7b400b0b8913fe0fb5b0734a6b08
Phillip Chazares <pchazares@ggc.edu>
2019-11-12 14:58:34 -0500

    made changes so all the counters can add or subtract

--- src/pages/Game/CommanderCost.js
+++ src/pages/Game/CommanderCost.js
@@ -6,15 +6,28 @@ import Decrement from "./Decrement";
 class CommanderCost extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { commanderCost : 0};
+        // in order to pass data from parent to child or child to parent ypu need to initialize you state and properties
+        this.state = { counter : 0, hitPoints: 0}
+    }
+
+    //create your getter function
+    getCounter = (newCounter) => {
+        //set your new state from your current state structure
+        let newState = this.state;
+        // now change the properties vairables
+        newState = { counter: newCounter,
+            hitPoints: newState.hitPoints }
+        // now set your you state to update it
+        this.setState(newState);
+
     }
     render(){
         return (
             <div className={"commander-cost"}>
                 <h5 className={"title"}>Commander Cost</h5>
-                <Increment hitPoints={2}/>
-                <h1 className={"life-points"}>{this.state.commanderCost}</h1>
-                <Decrement hitPoints={2}/>
+                <Increment sendCounter={this.getCounter} counter={this.state.counter} hitPoints={1}/>
+                <h1 className={"commander-cost-points"}>{this.state.counter}</h1>
+                <Decrement sendCounter={this.getCounter} counter={this.state.counter} hitPoints={1}/>
             </div>
         );
     }
