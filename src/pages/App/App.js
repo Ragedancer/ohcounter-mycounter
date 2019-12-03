6b01f06d68b5953e94b02f1dad2766c1a93ea2ed
Weston <warmstrong2@ggc.edu>
2019-12-03 11:13:49 -0500

    added comments for comments

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -15,14 +15,15 @@ const Hamburger = () => (
             <div className="wrapper">
                 <div className="content">
                     <ul>
-                        <li><Link to="/">Action Prompt</Link></li>
 
-                        {/* <li><Link to="/numplayerselect">Select Player</Link></li>
-                        <li><Link to="/gameselect">Game Select</Link></li> */}
+                        <li><Link to="/">Action Prompt</Link></li>
                         <li><Link to="/game">Game</Link></li>
-                        {/* <li><Link to="/playername">Player Name</Link></li> */}
                         <li><Link to="/randomplayer">Random Player Selector</Link></li>
                         <li><Link to="/dice">Dice</Link></li>
+                        {/* Created for development reasons
+                        <li><Link to="/numplayerselect">Select Player</Link></li>
+                        <li><Link to="/gameselect">Game Select</Link></li>
+                        <li><Link to="/playername">Player Name</Link></li> */}
                     </ul>
                 </div>
                 <div className="parent">Menu</div>
@@ -35,9 +36,10 @@ const Hamburger = () => (
             <Route path="/game" component={Game}/>
             <Route path={"/randomplayer"} component={RandomPlayerSelector}/>
             <Route path="/gameselect" component={GameSelect}/>
-            {/* <Route path="/numplayerselect" Component={}/> */}
-            {/* <Route path="/playername" Component={}/> */}
-            {/* <Route path="/gameselect" component={GameSelect}/> */}
+            {/* Created for development Reasons
+            <Route path="/numplayerselect" Component={}/>
+            <Route path="/playername" Component={}/>
+            <Route path="/gameselect" component={GameSelect}/> */}
 
         
     </Router>
